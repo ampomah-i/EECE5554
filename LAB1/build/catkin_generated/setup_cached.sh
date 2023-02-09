ef7ef4a4c81e4ded0aef9ba98d973a2a6b2ba603
ampomah-i <ampomah.i@northeastern.edu>
2023-02-08 23:12:11 -0500

    folder restructure

--- LAB1/build/catkin_generated/setup_cached.sh
+++ LAB1/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,10 @@
 # new environment variables
 
 # modified environment variables
-export PWD='/home/immanuel/EECE5554/Structure_Checker/EECE5554/LAB1/build'
+export CMAKE_PREFIX_PATH='/home/immanuel/EECE5554/LAB1/devel:/opt/ros/noetic'
+export LD_LIBRARY_PATH='/home/immanuel/EECE5554/LAB1/devel/lib:/opt/ros/noetic/lib'
+export PKG_CONFIG_PATH='/home/immanuel/EECE5554/LAB1/devel/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig'
+export PWD='/home/immanuel/EECE5554/LAB1/build'
+export PYTHONPATH='/home/immanuel/EECE5554/LAB1/devel/lib/python3/dist-packages:/opt/ros/noetic/lib/python3/dist-packages'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/immanuel/EECE5554/LAB1/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/immanuel/EECE5554/LAB1/src:/opt/ros/noetic/share'
